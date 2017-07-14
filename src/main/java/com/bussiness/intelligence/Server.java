b99ae99d283c893c1851ab7156612c37bdc17cca
niujiaxi <68394845@163.com>
2017-07-14 16:42:09 +0800

    iok

---
+++ src/main/java/com/bussiness/intelligence/Server.java
@@ -0,0 +1,29 @@
+package com.bussiness.intelligence;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * Created by yanshi on 2017/7/14.
+ */
+@EnableScheduling
+@Slf4j
+@SpringBootApplication
+public class Server {
+
+    private static ConfigurableApplicationContext context;
+
+    public static void main(String[] args) {
+        Server.context = SpringApplication.run(Server.class, args);
+    }
+
+    @PreDestroy
+    public void close() {
+        Server.context.close();
+    }
+}
