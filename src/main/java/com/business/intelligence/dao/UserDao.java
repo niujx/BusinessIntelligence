a14566fbab47f6a813d8b6aeef961800d77438ee
jingyang <zhoujingyang@gomeholdings.com>
2017-08-10 16:42:13 +0800

    增加调用饿了么方法

--- src/main/java/com/business/intelligence/dao/UserDao.java
+++ src/main/java/com/business/intelligence/dao/UserDao.java
@@ -7,7 +7,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zjy on 17/8/8.
@@ -33,6 +35,16 @@ public class UserDao {
 
     }
 
+
+
+    public User ifExists(String userName,String platformName){
+
+        Map sqlParam = new HashMap();
+        sqlParam.put("userName",userName);
+        sqlParam.put("platformName",platformName);
+        return sqlSessionTemplate.selectOne("com.business.intelligence.model.getUser",sqlParam);
+
+    }
 
 
 }
