2bdce3a79ba817f42ed80ca4a74a3be711f655af
王付坤 <wangfukun@gomeholdings.com>
2017-08-10 20:26:35 +0800

    update

--- src/main/resources/public/js/elemeCrawler.js
+++ src/main/resources/public/js/elemeCrawler.js
@@ -3,25 +3,33 @@
 	var address = "http://localhost:9578/bi/eleme/";
 	/**************************饿了么*******************************/
 	//点击开抓按钮时
-	$(".eleme .begin").on('click',function(){
-		//获取当前抓取时间
-		var curTime = $(this).parents("tr").find(".dateTime").val();
+	$(".eleme .crawlerOrder").on('click',function(){
+		
+		//获取当前抓取时间及用户名
+		var startDateTime = $(this).parents("tr").find(".startDateTime").val();
+		var endDateTime = $(this).parents("tr").find(".endDateTime").val();
+		var merchantUserName =  $(this).parents("tr").find(".merchantUserName").val();
 		//发送请求
 		$.ajax({
-			url:"./new_file.json",//请求路径
+
+			url:address + "crawlerOrder",//请求路径
 			type:"get",//请求方式 get和post
 			dataType:"json",//返回数据的格式
 			data:{
-				//给后台发送数据	抓取时间	beginDate是要传给后台的字段名 可换 ,curTime是抓取时间
-				beginDate: curTime 
+				//给后台发送数据	抓取时间	startTime等是要传给后台的字段名 可换 ,startDateTime是抓取开始时间
+				startTime: startDateTime,
+				endTime: endDateTime,
+				userName: merchantUserName
 			},
 			success:function(e){
 				//e 为请求成功后返回的数据
-				//console.log(e)
-				$(this).parents("tr").find(".getTime").text();//text(括号里放返回的日期)
+				console.log(e);
+				alert(e);
+//				$(this).parents("tr").find(".getTime").text();//text(括号里放返回的日期)
 			},
 			error:function(){
 				//请求失败时的函数
+				
 				console.log("请求失败");//控制台输出请求失败
 			}
 		})
