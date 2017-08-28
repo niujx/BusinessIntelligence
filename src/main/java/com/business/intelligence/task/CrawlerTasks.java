676c544e49c94427cdedcba511016a41fb28fa91
niujiaxi <68394845@163.com>
2017-08-28 11:31:13 +0800

    ok

--- src/main/java/com/business/intelligence/task/CrawlerTasks.java
+++ src/main/java/com/business/intelligence/task/CrawlerTasks.java
@@ -60,7 +60,6 @@ public class CrawlerTasks {
         for (Authenticate authenticate : authenticates) {
             MTCrawler.LoginBean loginBean = new MTCrawler.LoginBean();
             loginBean.setAuthenticate(authenticate);
-            mtCrawler.setAuthenticate(authenticate);
             mtCrawler.setLoginBean(loginBean);
             mtCrawler.login();
             mtCrawler.bizDataReport(startTime, endTime, false);
